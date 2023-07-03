86fa8153ed453918caca092e3e9eb382e9fa878d
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-07-03 15:51:02 +0300

    add-hw-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
+//Import
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+//Code
 console.log(galleryItems);
+
+console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+   </a>
+</li>`).join('');  
+}
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+
+ new SimpleLightbox('.gallery a', { captionDelay: 250 });
